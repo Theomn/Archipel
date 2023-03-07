5b8404e6cbc61a964d534cbdc1ec248a95c28935
Theo <theo_menard@hotmail.fr>
2023-03-07 16:43:50 -0500

    Boundary Line

---
+++ Assets/3_Scripts/System/BoundaryFromLine.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryFromLine : MonoBehaviour
+{
+    [SerializeField] private bool invertDirection;
+    [SerializeField] private bool loop;
+    [SerializeField] private bool showCollider;
+    [SerializeField] private GameObject wallMesh;
+    private LineRenderer line;
+
+    private void Awake()
+    {
+        line = GetComponent<LineRenderer>();
+    }
+
+    private void Start()
+    {
+        Vector3[] pos = new Vector3[line.positionCount];
+        line.GetPositions(pos);
+        for(int i = 0; i < line.positionCount-1; i++)
+        {
+            PlaceBoundaryBetween(pos[i], pos[i+1]);
+        }
+        if (loop)
+        {
+            PlaceBoundaryBetween(pos[pos.Length -1], pos[0]);
+        }
+        line.enabled = false;
+    }
+
+    private void PlaceBoundaryBetween(Vector3 a, Vector3 b)
+    {
+        var wall = Instantiate(wallMesh, transform);
+        var half = (b - a) / 2;
+        wall.transform.localPosition = a + half;
+        var a2 = new Vector2(a.x, a.z);
+        var b2 = new Vector2(b.x, b.z);
+        wall.transform.rotation = Quaternion.LookRotation(half);
+        wall.transform.eulerAngles = new Vector3(0, wall.transform.eulerAngles.y + (invertDirection? 90 : -90), 0);
+        wall.transform.localScale = new Vector3(Vector3.Distance(a2, b2), 1, 1);
+        if (showCollider)
+        {
+            wall.GetComponentInChildren<MeshRenderer>().enabled = true;
+        }
+    }
+}
