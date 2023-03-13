9fa5d941bc3a074d7a5a7e7340fb1d5b074a6433
Theo <theo_menard@hotmail.fr>
2023-03-13 19:37:19 -0400

    Wwise integration pt 1

--- Assets/3_Scripts/Items/Receptacle.cs
+++ Assets/3_Scripts/Items/Receptacle.cs
@@ -6,6 +6,7 @@ public class Receptacle : MonoBehaviour, Grabbable
 {
     public Item heldItem{get; private set;}
     [SerializeField] private Transform target;
+    [SerializeField] private AK.Wwise.Event blabla;
 
     public bool isHoldingItem
     {
@@ -15,10 +16,12 @@ public class Receptacle : MonoBehaviour, Grabbable
 
     public Vector3 Place(Item item)
     {
+        blabla.Post(gameObject);
         heldItem = item;
         item.LiftSprite(target.position.y - transform.position.y);
         item.GetComponent<Collider>().enabled = false;
         return target.position;
+
     }
 
     public Item Grab()
