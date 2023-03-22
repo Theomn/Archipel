9efa626f0d5bc72dbc94af3ebce57dfcf67b0518
Theo <theo_menard@hotmail.fr>
2023-03-21 21:01:42 -0400

    Prefab cleanup + Item elevator + WIP Oven

--- Assets/3_Scripts/Items/ItemElevator.cs
+++ Assets/3_Scripts/Items/ItemElevator.cs
@@ -8,18 +8,38 @@ public class ItemElevator : MonoBehaviour, Useable
     [SerializeField] private Transform receptacle;
     [SerializeField] private Transform top;
     [SerializeField] private Transform bottom;
+    [SerializeField] private Transform rope;
 
     private bool isUp;
     private bool pending;
 
+    private Sequence liftSequence, lowerSequence;
+
     private void Start()
     {
         receptacle.position = bottom.position;
+
+        Utils.SetHighSprite(rope, 1);
+        rope.position += Vector3.up + Vector3.forward;
+
+        liftSequence = DOTween.Sequence().Pause().SetAutoKill(false);
+        liftSequence.Append(receptacle.DOMoveY(bottom.position.y + 3f, 2f).SetEase(Ease.InSine))
+        .Append(receptacle.DOMove(top.position - Vector3.up * 3f, 0.01f))
+        .Append(receptacle.DOMoveY(top.position.y, 2f).SetEase(Ease.OutSine))
+        .OnComplete(ToggleState);
+
+        lowerSequence = DOTween.Sequence().Pause().SetAutoKill(false);
+        lowerSequence.Append(receptacle.DOMoveY(top.position.y - 3f, 2f).SetEase(Ease.InSine))
+        .Append(receptacle.DOMove(bottom.position + Vector3.up * 3f, 0.01f))
+        .Append(receptacle.DOMoveY(bottom.position.y, 2f).SetEase(Ease.OutSine))
+        .OnComplete(ToggleState);
     }
 
     public void Use()
     {
         if (pending) return;
+
+        rope.DOMoveY(rope.position.y - 0.2f, 0.3f).SetLoops(2, LoopType.Yoyo);
         if (isUp) Lower();
         else Lift();
     }
@@ -27,21 +47,17 @@ public class ItemElevator : MonoBehaviour, Useable
     private void Lift()
     {
         pending = true;
-        receptacle.DOKill();
-        receptacle.DOMoveY(receptacle.position.y + 2f, 2f).SetEase(Ease.InSine).OnComplete(GoTo);
+        liftSequence.Restart();
     }
 
     private void Lower()
     {
         pending = true;
-        receptacle.DOKill();
-        receptacle.DOMoveY(receptacle.position.y - 2f, 2f).SetEase(Ease.InSine).OnComplete(GoTo);
+        lowerSequence.Restart();
     }
 
-    private void GoTo()
+    private void ToggleState()
     {
-        if (isUp) receptacle.position = bottom.position;
-        else receptacle.position = top.position;
         isUp = !isUp;
         pending = false;
     }
