530ff9c8d8e6c2500bf92f73175a6a70fa7df498
Theo <theo_menard@hotmail.fr>
2023-03-27 17:10:51 -0400

    Readable stations

--- Assets/3_Scripts/Items/ReadableStation.cs
+++ Assets/3_Scripts/Items/ReadableStation.cs
@@ -6,10 +6,21 @@ public class ReadableStation : MonoBehaviour, Useable
 {
     [SerializeField] private string textKey;
     [SerializeField] private TextType textType;
+    [SerializeField] private string thoughtKey;
+    [SerializeField] private List<string> removeThoughtKeys;
     public void Use()
     {
         HUDController.instance.DisplayText(textType, textKey);
         ControlToggle.TakeControl(Close, Button.use, Button.grab, Button.jump);
+
+        if (thoughtKey != "")
+        {
+            ThoughtScreen.instance.AddThought(thoughtKey);
+        }
+        foreach(var key in removeThoughtKeys)
+        {
+            ThoughtScreen.instance.RemoveThought(key);
+        }
     }
 
     public void Close()
