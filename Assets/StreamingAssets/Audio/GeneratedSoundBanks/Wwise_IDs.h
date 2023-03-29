904ca5d3e025955b2cee511282dfef11c797131f
Frédérique Le Duc-Moreau <leducmoreaufred@gmail.com>
2023-03-29 13:30:46 -0400

    Soundbank update 02

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,11 +13,92 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID BOAT_START = 2698067234U;
+        static const AkUniqueID CANNOTDROP_ITEMS = 4134925042U;
+        static const AkUniqueID CRUSHER_SUCCESSFUL = 3759273032U;
+        static const AkUniqueID CRUSHER_UNSUCCESSFUL = 422108005U;
+        static const AkUniqueID DROP_ITEMS_ONGROUND = 2407203184U;
+        static const AkUniqueID DROP_ITEMS_ONSUPPORT = 2406101566U;
+        static const AkUniqueID EAT_BLUEFRUIT = 805545494U;
+        static const AkUniqueID EAT_COCONUT = 1309440649U;
+        static const AkUniqueID EAT_FRUIT = 3065801850U;
         static const AkUniqueID FS_PLAYER_JUMP_UP = 2334968449U;
+        static const AkUniqueID HAZE_MOVING = 2249521380U;
+        static const AkUniqueID INGAME_MENU_ON = 1034957780U;
+        static const AkUniqueID INGAME_MENU_OUT = 3949039273U;
+        static const AkUniqueID INSIDE_HUGE = 488628165U;
+        static const AkUniqueID INSIDE_SMALL = 2403279293U;
+        static const AkUniqueID LEVITATION_STONE_OFF = 199399196U;
+        static const AkUniqueID LEVITATION_STONE_ON = 4073070294U;
+        static const AkUniqueID LIFT_START = 4186111119U;
+        static const AkUniqueID LIFT_STOP = 4218803221U;
+        static const AkUniqueID MAGNETICWALL_OFF = 3269896205U;
+        static const AkUniqueID MAGNETICWALL_ON = 244868289U;
+        static const AkUniqueID MAIN_MENU_IN = 580468006U;
+        static const AkUniqueID MAIN_MENU_OUT = 3091888041U;
+        static const AkUniqueID MAP_LOADED = 1758594367U;
+        static const AkUniqueID MEDITATION_MODE_IN = 2040942133U;
+        static const AkUniqueID MEDITATION_MODE_OUT = 2866550492U;
+        static const AkUniqueID OUTSIDE = 438105790U;
+        static const AkUniqueID OVEN_BELLOWS = 3133062398U;
+        static const AkUniqueID OVEN_READY = 3758302363U;
+        static const AkUniqueID PARCHMENT_CLOSE = 893994586U;
+        static const AkUniqueID PARCHMENT_OPEN = 2766354782U;
+        static const AkUniqueID PICKUP_FRUIT_INBUSHES = 2226557690U;
+        static const AkUniqueID PICKUP_ITEMS = 417241228U;
+        static const AkUniqueID PLANT_UNWITHDRAW = 1715306164U;
+        static const AkUniqueID PLANT_WITHDRAW = 3054655441U;
+        static const AkUniqueID RAIN_START = 1665349938U;
+        static const AkUniqueID RAIN_STOP = 1737644906U;
+        static const AkUniqueID SOUNDCIN_01_START = 4287548115U;
+        static const AkUniqueID SOUNDCIN_01_STOP = 3222083729U;
+        static const AkUniqueID SOUNDCIN_02_START = 3931508232U;
+        static const AkUniqueID SOUNDCIN_02_STOP = 1199818068U;
+        static const AkUniqueID TELESCOPE_TICKING = 1394180663U;
+        static const AkUniqueID TREECOCONUT_FALLING = 149653470U;
+        static const AkUniqueID UI_APP_TEXT = 3388190351U;
+        static const AkUniqueID UI_IG_BUTTONCLICK = 3491422823U;
+        static const AkUniqueID UI_IG_BUTTONHOVER = 2437495857U;
+        static const AkUniqueID UI_IG_MENUBACK = 3703967629U;
+        static const AkUniqueID UI_IG_MENUFWD = 2286032095U;
+        static const AkUniqueID UI_IG_PARAMSCROLL = 70662291U;
+        static const AkUniqueID UI_IG_PARAMSELECT = 1532545184U;
+        static const AkUniqueID UI_IG_SLIDERSCROLL = 2407279249U;
+        static const AkUniqueID UI_MM_BUTTONCLICK = 2296312413U;
+        static const AkUniqueID UI_MM_BUTTONHOVER = 1886447351U;
+        static const AkUniqueID UI_MM_MENUBACK = 1648635023U;
+        static const AkUniqueID UI_MM_MENUFWD = 672841865U;
+        static const AkUniqueID UI_MM_PARAMSCROLL = 423427117U;
+        static const AkUniqueID UI_MM_PARAMSELECT = 917596458U;
+        static const AkUniqueID UI_MM_SLIDERSCROLL = 1939544035U;
+        static const AkUniqueID UI_NEW_THOUGHT = 2899380982U;
+        static const AkUniqueID VISION_END = 234923115U;
+        static const AkUniqueID VISION_START = 379684176U;
+        static const AkUniqueID WALL_MOVING = 2727895790U;
+        static const AkUniqueID ZONE_BEACH = 3272887647U;
+        static const AkUniqueID ZONE_HARBOUR = 1916089487U;
+        static const AkUniqueID ZONE_INTERSECTION = 137016265U;
+        static const AkUniqueID ZONE_OBSERVATORY = 3562476234U;
+        static const AkUniqueID ZONE_TEMPLE = 3796268957U;
+        static const AkUniqueID ZONE_TERRACE = 925872468U;
+        static const AkUniqueID ZONE_VILLAGE = 3927380446U;
+        static const AkUniqueID ZONE_VISTAS = 2992097724U;
     } // namespace EVENTS
 
     namespace STATES
     {
+        namespace CRUSHER
+        {
+            static const AkUniqueID GROUP = 457624467U;
+
+            namespace STATE
+            {
+                static const AkUniqueID LEVITATION_OFF = 4096035926U;
+                static const AkUniqueID LEVITATION_ON = 3835535640U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace CRUSHER
+
         namespace GAMEPLAY
         {
             static const AkUniqueID GROUP = 89505537U;
@@ -50,6 +131,31 @@ namespace AK
             } // namespace STATE
         } // namespace LOCATION
 
+        namespace RAIN
+        {
+            static const AkUniqueID GROUP = 2043403999U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID RAIN_OFF = 2312750017U;
+                static const AkUniqueID RAIN_ON = 462803477U;
+            } // namespace STATE
+        } // namespace RAIN
+
+        namespace ROOMTYPE
+        {
+            static const AkUniqueID GROUP = 1992536912U;
+
+            namespace STATE
+            {
+                static const AkUniqueID INSIDE_HUGE = 488628165U;
+                static const AkUniqueID INSIDE_SMALL = 2403279293U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID OUTSIDE = 438105790U;
+            } // namespace STATE
+        } // namespace ROOMTYPE
+
     } // namespace STATES
 
     namespace SWITCHES
@@ -72,13 +178,22 @@ namespace AK
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
         static const AkUniqueID TEST_SOUNDBANK = 4154996803U;
     } // namespace BANKS
 
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID SFX = 393239870U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID INSIDE_HUGE = 488628165U;
+        static const AkUniqueID INSIDE_SMALL = 2403279293U;
+        static const AkUniqueID OUTSIDE = 438105790U;
+    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
