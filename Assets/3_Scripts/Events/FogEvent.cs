dc895c1b2b238961b7c99d11ed8a3837d1710e98
Theo <theo_menard@hotmail.fr>
2023-04-01 11:51:45 -0400

    village fog and tweaks

--- Assets/3_Scripts/Events/FogEvent.cs
+++ Assets/3_Scripts/Events/FogEvent.cs
@@ -7,18 +7,16 @@ public class FogEvent : Event
 {
     [SerializeField] GameObject vista;
     [SerializeField] AK.Wwise.Event activateEvent;
-    private Material material;
 
     private void Awake()
     {
-        material = GetComponent<MeshRenderer>().material;
         if (vista) vista.SetActive(false);
     }
     public override void Activate()
     {
-        material.DOFade(0, 5);
         if (vista) vista.SetActive(true);
         activateEvent.Post(gameObject);
+        gameObject.SetActive(false);
     }
 
     public override void Deactivate()
