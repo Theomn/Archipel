2af74781e056786f2978fa11d7b70f1186ae9fa4
Frédérique Le Duc-Moreau <leducmoreaufred@gmail.com>
2023-04-02 19:24:19 -0400

    Soundbank Update

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -168,6 +168,7 @@ namespace AK
                 static const AkUniqueID GRASS = 4248645337U;
                 static const AkUniqueID SAND = 803837735U;
                 static const AkUniqueID STONE = 1216965916U;
+                static const AkUniqueID TALLGRASS = 659148630U;
                 static const AkUniqueID WATER = 2654748154U;
             } // namespace SWITCH
         } // namespace MATERIAL
