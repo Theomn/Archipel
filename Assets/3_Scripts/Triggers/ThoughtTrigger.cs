2d36dddd517324ff698964d6210e1e7223c8e056
Theo <theo_menard@hotmail.fr>
2023-04-15 12:08:28 -0400

    Thought Trigger

---
+++ Assets/3_Scripts/Triggers/ThoughtTrigger.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThoughtTrigger : MonoBehaviour
+{
+    [SerializeField] private List<string> addThoughtKeys;
+    [SerializeField] private List<string> removeThoughtKeys;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer != Layer.player) return;
+
+        foreach(string key in addThoughtKeys)
+        {
+            ThoughtScreen.instance.AddThought(key);
+        }
+        foreach(string key in removeThoughtKeys)
+        {
+            ThoughtScreen.instance.RemoveThought(key);
+        }
+
+        Destroy(gameObject);
+    }
+}
