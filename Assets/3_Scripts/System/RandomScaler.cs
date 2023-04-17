1f4d65deca3ca79512e369ae355b10e8c1659264
Theo <theo_menard@hotmail.fr>
2023-04-17 19:41:06 -0400

    Objects can now have a wind parameter

--- Assets/3_Scripts/System/RandomScaler.cs
+++ Assets/3_Scripts/System/RandomScaler.cs
@@ -7,16 +7,29 @@ public class RandomScaler : MonoBehaviour
 {
     [SerializeField] private float randomScaleMultiplierRange;
     [SerializeField] private float punchForce;
+    [SerializeField] private float windForce;
+
+    private Collider coll;
 
     private Quaternion startRotation;
     private void Awake()
     {
         transform.localScale *= 1 + Random.Range(-randomScaleMultiplierRange, +randomScaleMultiplierRange);
         startRotation = transform.rotation;
+        coll = GetComponent<Collider>();
+        if (windForce > 0 && (!coll || coll.isTrigger == false))
+        {
+            windForce = Random.value > 0.5f ? windForce : -windForce;
+            transform.Rotate(Vector3.forward * -windForce / 2);
+            var period = Random.Range(2f,4f);
+            transform.DORotate(Vector3.forward * windForce, period).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
+        }
     }
 
-    private void OnTriggerEnter(Collider other) {
-        if(other.gameObject.layer != Layer.player) return;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer != Layer.player) return;
+        if (punchForce <= 0) return;
 
         var punchDirection = Mathf.Sign(other.transform.position.x - transform.position.x);
         transform.DOKill();
