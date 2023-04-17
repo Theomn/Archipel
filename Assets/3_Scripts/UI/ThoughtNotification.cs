0a75b2ed6799778042a6d74a1bfbe173505996ae
Theo <theo_menard@hotmail.fr>
2023-04-17 19:24:22 -0400

    Thought notification fix

--- Assets/3_Scripts/UI/ThoughtNotification.cs
+++ Assets/3_Scripts/UI/ThoughtNotification.cs
@@ -11,7 +11,7 @@ public class ThoughtNotification : MonoBehaviour
     [SerializeField] private Image notificationIcon;
     [SerializeField] private Transform iconStart, iconEnd;
 
-    private Vector3 initialBubbleScale, initialIconScale;
+    private Vector3 initialBubbleScale, initialEndIconScale, initialStartIconScale;
     private Sequence bubbleSequence, notificationAppearSequence, notificationIdleSequence;
 
     private void Awake()
@@ -20,6 +20,10 @@ public class ThoughtNotification : MonoBehaviour
     }
     void Start()
     {
+        notificationIcon.transform.SetParent(iconEnd);
+        initialEndIconScale = notificationIcon.transform.localScale;
+        notificationIcon.transform.SetParent(iconStart);
+        initialStartIconScale = notificationIcon.transform.localScale;
         initialBubbleScale = bubble.transform.localScale;
         CreateSequences();
         Hide();
@@ -45,19 +49,20 @@ public class ThoughtNotification : MonoBehaviour
         notificationIcon.transform.localScale = Vector3.zero;
         bubble.transform.localScale = Vector3.zero;
 
-
+        notificationIdleSequence.Pause();
         notificationAppearSequence.Restart();
         notificationAppearSequence.onComplete += () =>
         {
             notificationIcon.transform.SetParent(iconEnd);
-            notificationIcon.transform.DOJump(iconEnd.position, 350, 1, 0.5f).SetEase(Ease.InOutSine).onComplete += () => 
+            notificationIcon.transform.DOScale(initialEndIconScale * 3f, 0.4f).SetEase(Ease.OutSine).SetLoops(2, LoopType.Yoyo);
+            notificationIcon.transform.DOJump(iconEnd.position, 350, 1, 0.8f).SetEase(Ease.InOutSine).onComplete += () => 
             //notificationIcon.transform.DOMove(iconEnd.position, 0.8f).SetEase(Ease.InOutSine).onComplete += () => 
             {
+                notificationIcon.transform.localScale = initialEndIconScale;
                 notificationIdleSequence.Restart();
             };
         };
         bubbleSequence.Restart();
-        notificationIdleSequence.Pause();
     }
 
     public void Hide()
@@ -74,7 +79,6 @@ public class ThoughtNotification : MonoBehaviour
         notificationAppearSequence.Kill();
         notificationIdleSequence.Kill();*/
 
-        initialIconScale = notificationIcon.transform.localScale;
         bubbleSequence = DOTween.Sequence().Pause().SetAutoKill(false);
         bubbleSequence.Append(bubble.transform.DOScale(initialBubbleScale, 0.4f).SetEase(Ease.OutBack));
         bubbleSequence.AppendInterval(1f);
@@ -82,13 +86,13 @@ public class ThoughtNotification : MonoBehaviour
 
         notificationIdleSequence = DOTween.Sequence().Pause().SetAutoKill(false);
         notificationIdleSequence.AppendInterval(2f);
-        notificationIdleSequence.Append(notificationIcon.transform.DOPunchScale(initialIconScale * 2.5f, 0.25f, 0, 0));
+        notificationIdleSequence.Append(notificationIcon.transform.DOPunchScale(initialEndIconScale, 0.25f, 0, 0));
         notificationIdleSequence.SetLoops(-1, LoopType.Restart);
 
         notificationAppearSequence = DOTween.Sequence().Pause().SetAutoKill(false);
         notificationAppearSequence.AppendInterval(0.4f);
-        notificationAppearSequence.Append(notificationIcon.transform.DOScale(initialIconScale * 2f, 0.3f).SetEase(Ease.InCirc));
-        notificationAppearSequence.Append(notificationIcon.transform.DOScale(initialIconScale, 0.3f).SetEase(Ease.OutSine));
+        notificationAppearSequence.Append(notificationIcon.transform.DOScale(initialStartIconScale * 2.5f, 0.3f).SetEase(Ease.InCirc));
+        notificationAppearSequence.Append(notificationIcon.transform.DOScale(initialStartIconScale, 0.3f).SetEase(Ease.OutSine));
         notificationAppearSequence.AppendInterval(0.4f);
         /*notificationAppearSequence.Append(notificationIcon.transform.DOMove(iconEnd.position, 0.5f).SetEase(Ease.InOutSine));
         notificationAppearSequence.onComplete += () =>
