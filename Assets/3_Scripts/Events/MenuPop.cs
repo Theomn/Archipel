61a40b8294ad3b4fa89bb5a0bea4b83f59f6cc37
Tiquoti <tiquoti@gmail.com>
2023-04-18 17:19:36 -0400

    Work on vista + tree import

---
+++ Assets/3_Scripts/Events/MenuPop.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPop : MonoBehaviour
+{
+    private float timer = -1;
+    [SerializeField] GameObject title;
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer sprite;
+
+    // Update is called once per frame
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == Layer.player)
+        {
+            timer = 4;
+              // animator.SetTrigger("Activate");
+        }
+
+
+
+    }
+    void Update()
+    {
+        // animator.SetTrigger("Activate");
+
+        if (timer >= 0)
+        {
+            timer -= Time.deltaTime;
+            if (timer <=0)
+            {
+                animator.SetTrigger("Activate");
+            }
+
+        }
+        
+    }
+}
